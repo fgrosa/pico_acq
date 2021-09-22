b291ebd0897a2f9842d5d8f38cb2d38ccc1926d6
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2021-09-22 15:34:42 +0200

    fix integer overflow problem in ADC conversion

--- utils.py
+++ utils.py
@@ -649,13 +649,13 @@ def read_channel_runblock(status, handle, resolution, sources, source_ranges, sa
 
     return waveform_mV, time
 
-def adc2mV_fast(bufferADC, range, maxADC):
+def adc2mV_fast(bufferADC, channel_range, maxADC):
 
     if not isinstance(bufferADC, np.ndarray):
         bufferADC = np.array(bufferADC)
 
     channelInputRanges = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000]
-    vRange = channelInputRanges[range]
-    bufferV = bufferADC * vRange / maxADC.value
+    vRange = channelInputRanges[channel_range]
+    bufferV = (1.0 * bufferADC) * vRange / maxADC.value
 
     return bufferV
